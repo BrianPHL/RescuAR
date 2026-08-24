d260db97646dbb10bd7968a444352af0f4120056
Brian <brianpasco1206@gmail.com>
2026-08-24 22:28:33 +0800

    feat(Platforms/Android/Authentication): add WebAuthenticator callback Activity

---
+++ RescuAR.MAUI/Platforms/Android/Authentication/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+
+namespace RescuAR.MAUI.Platforms.Android.Authentication;
+
+[Activity(
+    NoHistory = true,
+    LaunchMode = LaunchMode.SingleTop,
+    Exported = true)]
+[IntentFilter(
+    new[]
+    {
+        Intent.ActionView
+    },
+    Categories = new[]
+    {
+        Intent.CategoryDefault,
+        Intent.CategoryBrowsable
+    },
+    DataScheme = CallbackScheme)]
+public class WebAuthenticationCallbackActivity
+    : Microsoft.Maui.Authentication.WebAuthenticatorCallbackActivity
+{
+    private const string CallbackScheme =
+        "rescuar";
+}
