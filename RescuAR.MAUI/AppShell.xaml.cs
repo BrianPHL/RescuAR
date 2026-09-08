8fbd2545666b087d66e6189e0fcb0457f1ead9a0
Brian <brianpasco1206@gmail.com>
2026-09-08 15:15:41 +0800

    refactor(AppShell.xaml.cs): merge the AppShell.xaml.cs from the previous platform to the new platform

--- RescuAR.MAUI/AppShell.xaml.cs
+++ RescuAR.MAUI/AppShell.xaml.cs
@@ -42,17 +42,53 @@ public partial class AppShell : Shell
             typeof(RescuAR.App.Views.Reports.AdvisoryFeedPage));
 
         Routing.RegisterRoute(
-            "Reports/CommunityPosting",
-            typeof(RescuAR.App.ViewModels.Reports.CommunityPostingPage));
-
-        Routing.RegisterRoute(
             "ReportDetails",
             typeof(RescuAR.App.Views.Reports.ReportDetailsPage));
+
+        Routing.RegisterRoute(
+            "NotificationsPage",
+            typeof(RescuAR.App.Views.Reports.NotificationsPage));
 
         // Profile
         Routing.RegisterRoute(
             "ProfilePage",
             typeof(RescuAR.App.Views.Profile.ProfilePage));
+
+        Routing.RegisterRoute(
+            "PersonalInformationPage",
+            typeof(RescuAR.App.Views.Profile.PersonalInformationPage));
+
+        Routing.RegisterRoute(
+            "HealthInformationPage",
+            typeof(RescuAR.App.Views.Profile.HealthInformationPage));
+
+        Routing.RegisterRoute(
+            "SafetyCircleSettingsPage",
+            typeof(RescuAR.App.Views.Profile.SafetyCircleSettingsPage));
+
+        Routing.RegisterRoute(
+            "EmergencyContactsPage",
+            typeof(RescuAR.App.Views.Profile.EmergencyContactsPage));
+
+        Routing.RegisterRoute(
+            "AppSettingsPage",
+            typeof(RescuAR.App.Views.Profile.AppSettingsPage));
+
+        Routing.RegisterRoute(
+            "HelpCenterPage",
+            typeof(RescuAR.App.Views.Profile.HelpCenterPage));
+
+        Routing.RegisterRoute(
+            "PrivacyPolicyPage",
+            typeof(RescuAR.App.Views.Profile.PrivacyPolicyPage));
+
+        Routing.RegisterRoute(
+            "TermsConditionsPage",
+            typeof(RescuAR.App.Views.Profile.TermsConditionsPage));
+
+        Routing.RegisterRoute(
+            "SystemInformationPage",
+            typeof(RescuAR.App.Views.Profile.SystemInformationPage));
 
         // Summary
         Routing.RegisterRoute(
